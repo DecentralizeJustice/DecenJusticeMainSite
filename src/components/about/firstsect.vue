c52bbca80e4ecef3c67bc0d83015bf8179615e3f
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-04-19 13:42:29 -0700

    Working Resposnive

--- src/components/about/firstsect.vue
+++ src/components/about/firstsect.vue
@@ -1,36 +1,57 @@
 <template>
-<div class='hidden-sm-and-down'>
-    <v-flex xs12 class="ma-0 pa-0 second" style="height:100vh;">
-      <v-layout row wrap>
-        <v-flex xs9 style="height:100vh;">
+<div>
+  <div class='hidden-sm-and-down'>
+      <v-flex xs12 class="ma-0 pa-0 second" style="height:100vh;">
+        <v-layout row wrap>
+          <v-flex xs9 style="height:100vh;">
 
-          <div style="width:100%;
-          position:relative;left: 2%;top: 8%;">
+            <div style="width:100%;
+            position:relative;left: 2%;top: 8%;">
 
-            <div  class="text-xs-center"
-              style="width:70%;font-size:10vh;color: white;
-              position:relative;left: 14%;top: 80%;font-weight:bold;"
-            >What Drives Us</div>
+              <div  class="text-xs-center"
+                style="width:70%;font-size:10vh;color: white;
+                position:relative;left: 14%;top: 80%;font-weight:bold;"
+              >What Drives Us</div>
 
-              <div
-                style="width: 60vw;font-size: 3.5vh;color: white;
-                position:relative;left:10%;top: 4vh;
-                color: #09093B;text-align:left;" v-html="main"
-              >
-             </div>
-          </div>
-        </v-flex>
-        <v-flex xs3  style="height:100vh;">
-            <img src="https://res.cloudinary.com/dylevfpbl/image/upload/v1555482590/decenMain/undraw_bitcoin2_ave7.svg"
-              style="height:40vh;width:auto;position:relative;
-                left: 0%;
-                top: 30vh;" >
-        </v-flex>
-      </v-layout>
-    </v-flex>
-    <v-parallax style="height:50vh;"
-    src="https://res.cloudinary.com/dylevfpbl/image/upload/v1555450301/decenMain/mining-2648005_960_720.jpg">
-    </v-parallax>
+                <div
+                  style="width: 60vw;font-size: 3.5vh;
+                  position:relative;left:10%;top: 4vh;
+                  color: #09093B;text-align:left;" v-html="main"
+                >
+               </div>
+            </div>
+          </v-flex>
+          <v-flex xs3  style="height:100vh;">
+              <img src="https://res.cloudinary.com/dylevfpbl/image/upload/v1555482590/decenMain/undraw_bitcoin2_ave7.svg"
+                style="height:40vh;width:auto;position:relative;
+                  left: 0%;
+                  top: 30vh;" >
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-parallax style="height:50vh;"
+      src="https://res.cloudinary.com/dylevfpbl/image/upload/v1555450301/decenMain/mining-2648005_960_720.jpg">
+      </v-parallax>
+  </div>
+
+  <v-flex xs12 class="ma-0 pa-0 second hidden-md-and-up">
+
+      <div  class="text-xs-center"
+        style="width:100%;font-size:6vh;color: white;
+        position:relative;top: 3vh;font-weight:bold;"
+      >What Drives Us</div>
+
+        <div
+          style="width: 100vw;font-size: 3vh;
+          position:relative;top: 4vh;
+          color: #09093B;text-align:left;padding:3%;" v-html="main"
+        >
+       </div>
+
+    <img src="https://res.cloudinary.com/dylevfpbl/image/upload/v1555482590/decenMain/undraw_bitcoin2_ave7.svg"
+      style="height:auto;width:50vw;position:relative;margin-top:5vh;margin-bottom:5vh;" class="center">
+  </v-flex>
+
 </div>
 </template>
 <script>
@@ -51,22 +72,6 @@ export default {
     }
   },
   computed: {
-    titlefontsize () {
-      switch (this.$vuetify.breakpoint.name) {
-        case 'md': return '300%'
-        case 'sm': return '300%'
-        case 'xs': return '125%'
-        default: return '400%'
-      }
-    },
-    bodyfontsize () {
-      switch (this.$vuetify.breakpoint.name) {
-        case 'md': return '140%'
-        case 'sm': return '300%'
-        case 'xs': return '125%'
-        default: return '155%'
-      }
-    }
   }
 }
 </script>
@@ -77,11 +82,10 @@ export default {
 }
 
 .center{
-  margin: 0;
   position: relative;
-  top: 50%;
-  -ms-transform: translateY(-50%);
-  transform: translateY(-50%);
+  left: 50%;
+  -ms-transform: translateX(-50%);
+  transform: translateX(-50%);
 }
 
 </style>
