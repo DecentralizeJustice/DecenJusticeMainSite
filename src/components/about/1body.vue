7328926b77a0cf21b72da5d20f171123437128dc
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-04-19 00:55:07 -0700

    Trying to make responsive

---
+++ src/components/about/1body.vue
@@ -0,0 +1,65 @@
+<template>
+<div>
+    <v-flex xs12 class="ma-0 pa-0 second hidden-sm-and-down" style="height:80vh;">
+      <v-layout row wrap>
+        <v-flex md6 xs12 style="height:100vh;">
+
+          <div style="width:100%;
+          position:relative;left: 2%;top: 8%;">
+
+            <div  class="text-xs-center"
+              style="width:70%;font-size:400%;color: white;
+              position:relative;left: 20%;top: 8%;font-weight:bold;"
+            >What Drives Us</div>
+
+              <div  class=""
+                style="width: 100%;font-size: 200%;color: white;
+                position:relative;left:15%;top: 10%;
+                color: #09093B;text-align:left;"
+              >
+              We first got involved in crypto in 2014 by building mining rigs. To be in the space at this time required a
+              vast understanding of technology. Since then we have seen the crypto consumer space grow at
+              an exponential rate. Although the exponential for decentralized technologies has grown
+              since then, the tooling has not kept up with this growth. Most of the tools that exist
+              today are too advanced to be safe or to simple to be powerful. We are
+               working to bridge this gap with targeted applications that aim to empower,
+               not overwhelm or limit, users of decentralized technologies.
+             </div>
+          </div>
+        </v-flex>
+        <v-flex md6 xs12 style="height:25vh;">
+            <img src="https://res.cloudinary.com/dylevfpbl/image/upload/v1555482590/decenMain/undraw_bitcoin2_ave7.svg"
+              style="height:auto;width:50%;position:relative;
+                left: 25%;
+                top: 50%;" >
+        </v-flex>
+      </v-layout>
+    </v-flex>
+    <v-parallax
+    src="https://res.cloudinary.com/dylevfpbl/image/upload/v1555450301/decenMain/mining-2648005_960_720.jpg">
+    </v-parallax>
+</div>
+</template>
+<script>
+export default {
+  name: 'fbody',
+  components: {
+
+  }
+}
+</script>
+<style scoped>
+
+.second{
+  background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);
+}
+
+.center{
+  margin: 0;
+  position: relative;
+  top: 50%;
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+}
+
+</style>
--- src/views/about.vue
+++ src/views/about.vue
@@ -7,47 +7,13 @@
 
           We create apps that <span style="text-decoration: underline;font-weight:bold;">empower</span> people
           to use the full potential of <span style="text-decoration: underline;font-weight:bold;">decentralized tech</span>
-          
+
         </div>
     </v-flex>
-<!--
-        <v-flex xs12 class="ma-0 pa-0 second">
-          <v-layout row wrap>
-            <v-flex xs6 style="height:100%;">
-              <div class="" style="width:40vw;letter-spacing:.01em;font-size: 4.5em;color: white;position:relative;
-                left: 10vw;
-                top: 10vh;
-                font-weight:bold;"
-              >What Drives Us</div>
-              <div class="" style="width:50vw;font-size: 1.7em;position:relative;
-                left: 5vw;
-                top: 10vw;
-                text-align: left;
-                color: #09093B;"
-              >
-              We first got involved in crypto in 2014 by building mining rigs. To be in the space at this time required a
-              vast understanding of technology. Since then we have seen the crypto consumer space grow at
-              an exponential rate. Although the exponential for decentralized technologies has grown
-              since then, the tooling has not kept up with this growth. Most of the tools that exist
-              today are too advanced to be safe or to simple to be powerful. We are
-               working to bridge this gap with targeted applications that aim to empower,
-               not overwhelm or limit, users of decentralized technologies.
-            </div>
-            </v-flex>
-            <v-flex xs6 style="height:80vh;">
-                <img src="https://res.cloudinary.com/dylevfpbl/image/upload/v1555482590/decenMain/undraw_bitcoin2_ave7.svg"
-                  style="height:auto;width:20vw;position:relative;
-                    left: 18vw;
-                    top: 20vh;" >
-            </v-flex>
-          </v-layout>
-        </v-flex>
 
-        <v-parallax
-        src="https://res.cloudinary.com/dylevfpbl/image/upload/v1555450301/decenMain/mining-2648005_960_720.jpg">
-        </v-parallax>
+    <fbody/>
 
-        <v-flex xs12 class="ma-0 pa-0 third">
+        <!-- <v-flex xs12 class="ma-0 pa-0 third">
           <v-layout row wrap>
             <v-flex xs6 style="height:100%;">
               <div class="" style="width:40vw;letter-spacing:.01em;font-size: 4.5em;color: white;position:relative;
@@ -124,11 +90,12 @@
 </template>
 <script>
 import toolBar from '@/components/toolBar.vue'
-
+import fbody from '@/components/about/1body.vue'
 export default {
   name: 'about',
   components: {
-    toolBar
+    toolBar,
+    fbody
   },
   computed: {
     bannerfontSize () {
@@ -136,6 +103,29 @@ export default {
         case 'sm': return '3em'
         case 'xs': return '2em'
         default: return '5em'
+      }
+    },
+    sectionheight () {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'sm': return '125vh'
+        case 'xs': return '125vh'
+        default: return '80vh'
+      }
+    },
+    titlefontsize () {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'md': return '300%'
+        case 'sm': return '300%'
+        case 'xs': return '125%'
+        default: return '400%'
+      }
+    },
+    bodyfontsize () {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'md': return '140%'
+        case 'sm': return '300%'
+        case 'xs': return '125%'
+        default: return '155%'
       }
     }
   }
@@ -151,11 +141,9 @@ export default {
   background-size: cover;
 }
 .second{
-  height: 80vh;
   background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);
 }
 .third{
-  height: 80vh;
   background-color: #b1bfd8;
   background-image: linear-gradient(315deg, #b1bfd8 0%, #6782b4 74%);
 }
